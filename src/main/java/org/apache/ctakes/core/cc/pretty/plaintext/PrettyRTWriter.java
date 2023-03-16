09e4a04fcc2d479094fcd7d9fc96ff643411bbe4
Eli Goldner <egoldner@brandeis.edu>
2023-03-16 15:34:04 -0400

    Updates for less PBJ crossover and building with Maven

--- src/main/java/org/apache/ctakes/core/cc/pretty/plaintext/PrettyRTWriter.java
+++ src/main/java/org/apache/ctakes/core/cc/pretty/plaintext/PrettyRTWriter.java
@@ -1,8 +1,11 @@
-package org.apache.ctakes.rt_parser.cc.pretty.csv;
+package org.apache.ctakes.core.cc.pretty.plaintext;
 
+import org.apache.ctakes.core.util.Pair;
 import org.apache.ctakes.core.util.doc.DocIdUtil;
 import org.apache.ctakes.typesystem.type.textsem.ProcedureMention;
+import org.apache.ctakes.typesystem.type.textspan.Sentence;
 import org.apache.log4j.Logger;
+import org.apache.uima.analysis_component.JCasAnnotator_ImplBase;
 import org.apache.uima.fit.util.JCasUtil;
 import org.apache.uima.jcas.JCas;
 import org.apache.uima.jcas.tcas.Annotation;
@@ -11,8 +14,7 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 
@@ -21,14 +23,14 @@ import java.util.stream.Collectors;
  *
  * @author Eli , chip-nlp
  * @version %I%
- * @since 3/10/2023
+ * @since 3/13/2023
  */
-final public class PrettyCSVWriter {
+final public class PrettyRTWriter extends JCasAnnotator_ImplBase {
 
 
-   static private final Logger LOGGER = Logger.getLogger( "PrettyCSVWriter" );
-   static private final String FILE_EXTENSION = ".csv";
-   // static private int _current_procedure = 0;
+   static private final Logger LOGGER = Logger.getLogger( "PrettyRTWriter" );
+   static private final String FILE_EXTENSION = ".txt";
+   static private int _current_procedure = 0;
 
    private String _outputDirPath;
 
@@ -77,49 +79,43 @@ final public class PrettyCSVWriter {
 
    public void writeFile( final JCas jCas, final String outputFilePath ) {
       try ( final BufferedWriter writer = new BufferedWriter( new FileWriter( outputFilePath ) ) ) {
-         // csv columns
-         // writer.write("procedure_number,central_dose,boost,date,secondary_dose,fraction_frequency,fraction_number,site");
-         writer.write("central_dose,boost,date,secondary_dose,fraction_frequency,fraction_number,site");
-         writer.newLine();
-         JCasUtil.select( jCas, ProcedureMention.class )
+         Map<Annotation, Collection<Sentence>> sentIndex = JCasUtil.indexCovering(jCas, Annotation.class, Sentence.class);
+         final List<ProcedureMention> procedureMentions = JCasUtil
+                 .select( jCas, ProcedureMention.class )
                  .stream()
                  .sorted(
                          Comparator.comparing(
                                  ProcedureMention :: getBegin
                          )
                  )
-                 .forEach(p -> {
-                    try {
-                       writeMention(p, writer);
-                    } catch (IOException e) {
-                       // don't know why we didn't have to
-                       // deal with an exception possibility in the for loop but whatever
-                       throw new RuntimeException(e);
-                    }
-                 });
+                 .collect(Collectors.toList());
+         for ( ProcedureMention procedureMention : procedureMentions ) {
+            Sentence container = sentIndex.get(procedureMention).iterator().next();
+            writeMention( container, procedureMention, writer );
+         }
       } catch ( IOException ioE ) {
          LOGGER.error( "Could not not write csv file " + outputFilePath );
          LOGGER.error( ioE.getMessage() );
       }
    }
 
-   static public String getSpan( final Annotation attribute ){
-      return String.format(
-              "%d_%d,",
-              attribute.getBegin(),
-              attribute.getEnd()
-              );
+
+
+   static public Pair<Integer> getSpan( final Annotation attribute ){
+      return new Pair<>(attribute.getBegin(), attribute.getEnd());
    }
 
 
    /**
     * Write a sentence from the document text
     *
+    * @param container sentence containing the annotation
     * @param procedureMention annotation containing the procedure mention
     * @param writer   writer to which pretty text for the sentence should be written
     * @throws IOException if the writer has issues
     */
    static public void writeMention(
+           final Sentence container,
            final ProcedureMention procedureMention,
            final BufferedWriter writer
    ) throws IOException {
@@ -127,60 +123,89 @@ final public class PrettyCSVWriter {
       // there HAS to be a better way
       // (would be trivial if cTAKES returned a null instead of throwing a fit)
       // to do this but for now:
-      String youGotTheDud = "None,"; //or maybe -1_-1 if one runs into Pandas issues
 
-      // writer.write(String.format("%d,", _current_procedure));
-      // _current_procedure++;
 
+      writer.write(String.format("\n%d.\t", _current_procedure));
+      _current_procedure++;
+
+      Map<Pair<Integer>, String> labelToInds = new HashMap<>();
       // central dose
       try {
-         writer.write(getSpan(procedureMention));
-      } catch (Exception e){
-         // this should NOT happen, need to find the right exception
-         writer.write(youGotTheDud);
+         labelToInds.put(getSpan(procedureMention), "central-dose");
+      } catch (Exception ignored){
       }
-
       // boost
       try {
-         writer.write(getSpan(procedureMention.getStatusChange()));
-      } catch (Exception e){
-         writer.write(youGotTheDud);
+         labelToInds.put(getSpan(procedureMention.getStatusChange()), "boost");
+      } catch (Exception ignored){
       }
 
       // date
       try {
-         writer.write(getSpan(procedureMention.getStartTime()));
-      } catch (Exception e){
-         writer.write(youGotTheDud);
+         labelToInds.put(getSpan(procedureMention.getStartTime()), "date");
+      } catch (Exception ignored){
       }
 
       // secondary dose
       try {
-         writer.write(getSpan(procedureMention.getTotalDose()));
-      } catch (Exception e){
-         writer.write(youGotTheDud);
+         labelToInds.put(getSpan(procedureMention.getTotalDose()), "secondary-dose");
+      } catch (Exception ignored){
       }
 
       // fraction frequency
       try {
-         writer.write(getSpan(procedureMention.getFrequency()));
-      } catch (Exception e){
-         writer.write(youGotTheDud);
+         labelToInds.put(getSpan(procedureMention.getFrequency()), "fx-freq");
+      } catch (Exception ignored){
       }
 
       // fraction number
       try {
-         writer.write(getSpan(procedureMention.getDosageCount()));
-      } catch (Exception e){
-         writer.write(youGotTheDud);
+         labelToInds.put(getSpan(procedureMention.getDosageCount()), "fx-no");
+      } catch (Exception ignored){
       }
 
       // site
       try {
-         writer.write(getSpan(procedureMention.getAnatomicalSite()));
-      } catch (Exception e){
-         writer.write(youGotTheDud);
+         labelToInds.put(getSpan(procedureMention.getAnatomicalSite()), "site");
+      } catch (Exception ignored){
       }
+
+      writer.write(
+              taggedSentence(
+              container
+                      .getCoveredText()
+                      .replace("\n", " "),
+              labelToInds
+         )
+      );
       writer.newLine();
+   }
+
+   static private String taggedSentence(String sentenceText, Map<Pair<Integer>, String> labelToInds){
+      StringBuilder out = new StringBuilder();
+      List<Pair<Integer>> orderedInds = labelToInds
+              .keySet()
+              .stream()
+              .sorted(
+                      Comparator
+                              .comparing(
+                                      Pair::getValue1
+                              )
+              ).collect(
+                      Collectors.toList()
+              );
+
+      int previous = 0;
+
+      for (Pair<Integer> indices : orderedInds){
+         String tag = labelToInds.get(indices);
+         out.append(sentenceText, previous, indices.getValue1());
+         out.append(String.format("<%s>", tag));
+         out.append(sentenceText, indices.getValue1(), indices.getValue2());
+         out.append(String.format("</%s>", tag));
+         previous = indices.getValue2();
+      }
+
+      return out.toString();
    }
 }

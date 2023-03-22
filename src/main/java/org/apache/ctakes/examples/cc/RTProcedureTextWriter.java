376421f24b651cc8283b08362c39fb2ba797341f
Eli Goldner <egoldner@brandeis.edu>
2023-03-22 14:51:34 -0400

    Subdirectory logic finally working for the procedure text writer.

--- src/main/java/org/apache/ctakes/examples/cc/RTProcedureTextWriter.java
+++ src/main/java/org/apache/ctakes/examples/cc/RTProcedureTextWriter.java
@@ -1,19 +1,15 @@
 package org.apache.ctakes.examples.cc;
 
+import org.apache.ctakes.core.cc.AbstractJCasFileWriter;
 import org.apache.ctakes.core.util.Pair;
-import org.apache.ctakes.core.util.doc.DocIdUtil;
 import org.apache.ctakes.typesystem.type.textsem.ProcedureMention;
 import org.apache.ctakes.typesystem.type.textspan.Sentence;
 import org.apache.log4j.Logger;
-import org.apache.uima.analysis_component.JCasAnnotator_ImplBase;
 import org.apache.uima.fit.util.JCasUtil;
 import org.apache.uima.jcas.JCas;
 import org.apache.uima.jcas.tcas.Annotation;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -25,66 +21,28 @@ import java.util.stream.Collectors;
  * @version %I%
  * @since 3/13/2023
  */
-final public class RTProcedureTextWriter extends JCasAnnotator_ImplBase {
+final public class RTProcedureTextWriter extends AbstractJCasFileWriter {
 
 
     static private final Logger LOGGER = Logger.getLogger( "RTProcedureTextWriter" );
     static private final String FILE_EXTENSION = ".txt";
     static private int _current_procedure = 0;
 
-    private String _outputDirPath;
 
-    /**
-     * @param outputDirectoryPath may be empty or null, in which case the current working directory is used
-     * @throws IllegalArgumentException if the provided path points to a File and not a Directory
-     * @throws SecurityException        if the File System has issues
-     */
-    public void setOutputDirectory( final String outputDirectoryPath ) throws IllegalArgumentException,
-            SecurityException {
-        // If no outputDir is specified (null or empty) the current working directory will be used.  Else check path.
-        if ( outputDirectoryPath == null || outputDirectoryPath.isEmpty() ) {
-            _outputDirPath = "";
-            LOGGER.debug( "No Output Directory Path specified, using current working directory "
-                    + System.getProperty( "user.dir" ) );
-            return;
-        }
-        final File outputDir = new File( outputDirectoryPath );
-        if ( !outputDir.exists() ) {
-            outputDir.mkdirs();
-        }
-        if ( !outputDir.isDirectory() ) {
-            throw new IllegalArgumentException( outputDirectoryPath + " is not a valid directory path" );
-        }
-        _outputDirPath = outputDirectoryPath;
-        LOGGER.debug( "Output Directory Path set to " + _outputDirPath );
-    }
-
-    /**
-     * Process the jcas and write pretty sentences to file.  Filename is based upon the document id stored in the cas
-     *
-     * @param jcas ye olde ...
-     */
-    public void process( final JCas jcas ) {
-        LOGGER.info( "Starting processing" );
-        final String docId = DocIdUtil.getDocumentIdForFile( jcas );
-        File outputFile;
-        if ( _outputDirPath == null || _outputDirPath.isEmpty() ) {
-            outputFile = new File( docId + FILE_EXTENSION );
-        } else {
-            outputFile = new File( _outputDirPath, docId + FILE_EXTENSION );
-        }
-        writeFile( jcas, outputFile.getPath() );
-        LOGGER.info( "Finished processing" );
-    }
-
-    public void writeFile( final JCas jCas, final String outputFilePath ) {
-        try ( final BufferedWriter writer = new BufferedWriter( new FileWriter( outputFilePath ) ) ) {
+    @Override
+    public void writeFile( final JCas jCas,
+                           final String outputDir,
+                           final String documentId,
+                           final String fileName ) throws IOException {
+        final File outputFilePath = new File( outputDir , fileName + FILE_EXTENSION );
+        LOGGER.info("Writing " + fileName + FILE_EXTENSION + " to " + outputFilePath.getPath()  +" ...") ;
+        try ( Writer writer = new BufferedWriter( new FileWriter( outputFilePath ) ) ) {
             Map<Annotation, Collection<Sentence>> sentIndex = JCasUtil.indexCovering(jCas, Annotation.class, Sentence.class);
             final List<ProcedureMention> procedureMentions = JCasUtil
                     .select( jCas, ProcedureMention.class )
                     .stream()
                     .sorted(
-                            Comparator.comparing(
+                            Comparator.comparingInt(
                                     ProcedureMention :: getBegin
                             )
                     )
@@ -93,12 +51,15 @@ final public class RTProcedureTextWriter extends JCasAnnotator_ImplBase {
                 Sentence container = sentIndex.get(procedureMention).iterator().next();
                 writeMention( container, procedureMention, writer );
             }
-        } catch ( IOException ioE ) {
+        }
+        /*
+        catch ( IOException ioE ) {
             LOGGER.error( "Could not not write csv file " + outputFilePath );
             LOGGER.error( ioE.getMessage() );
         }
+         */
+        LOGGER.info( "Finished Writing" );
     }
-
 
 
     static public Pair<Integer> getSpan( final Annotation attribute ){
@@ -117,7 +78,7 @@ final public class RTProcedureTextWriter extends JCasAnnotator_ImplBase {
     static public void writeMention(
             final Sentence container,
             final ProcedureMention procedureMention,
-            final BufferedWriter writer
+            final Writer writer
     ) throws IOException {
 
         // there HAS to be a better way to do this
@@ -176,7 +137,7 @@ final public class RTProcedureTextWriter extends JCasAnnotator_ImplBase {
                         labelToInds
                 )
         );
-        writer.newLine();
+        writer.write("\n");
     }
 
     static private String taggedSentence(Sentence sentence, Map<Pair<Integer>, String> labelToInds){
